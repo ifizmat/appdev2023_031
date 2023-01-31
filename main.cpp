fabea80fdf556088c4a57f1e23dac21d0d132c50
ifizmat <ifizmat@gmail.com>
2023-01-31 09:20:11 +0400

    4. Car properties: X, P, V.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
 class Car {
-	
+	public:
+	  double X;
+	  int    P;
+	  double V;
 	
 };
 
@@ -9,5 +12,8 @@ int main() {
 	using namespace std;
 	cout << "Test v3.1: OK.\n";
 	Car car;
+	cout << "X: " << car.X << endl;
+	cout << "P: " << car.P << endl;
+	cout << "V: " << car.V << endl;
 	return 0;
 }
